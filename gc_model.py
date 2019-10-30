a0933e00b120ff183b99688d92a720e37d5b1531
Hagai Kariti <hkariti@gmail.com>
2019-10-30 14:45:08 +0200

    New generic model
    * Arbitrary number of compartments
    * Supports any state-state interaction probabily (A-A, A-B, etc)
    * Calculates gradient automatically using autograd
    * Includes a function to generate the interaction matrix using model params
    * Better separation of model code and main script

---
+++ gc_model.py
@@ -0,0 +1,197 @@
+from __future__ import division
+import autograd.numpy as np
+from autograd import grad
+import functools
+import scipy as sp
+from scipy import optimize
+
+def calculate_likelihood(interactions, non_nan_indices, number_of_states, probabilities_params_count, weights_param_count,
+        number_of_bins, variables):
+    """
+    Caulculate the log-likelihood of a given interaction (in interactions) using a model with a given number of states and
+    with the current values for all model parameters (state probabilities, distance decay power value, etc)
+
+    :return: The minus of the log-likelihood, to be used by minimization optimization algorithms.
+    """
+    prob_vars, weights_vars, dd_power = extract_variables(probabilities_params_count, weights_param_count, variables)
+    lower_triangle_indices = np.tril_indices(number_of_bins, -1)
+
+    distance_mat = np.absolute(non_nan_indices[None].T - non_nan_indices) + np.diag(np.full(number_of_bins, np.nan))
+    distance_vec = distance_mat[lower_triangle_indices]
+
+    state_probabilities_mat = normalize(prob_vars.reshape(number_of_bins, number_of_states), normalize_axis=1)
+    state_weights_mat = normalize(triangle_to_symmetric(number_of_states, weights_vars))
+    weighted_states_probabilities = np.dot(state_probabilities_mat, state_weights_mat)
+    state_interactions_model = np.dot(weighted_states_probabilities, state_probabilities_mat.T) 
+    state_interactions_vec = state_interactions_model[lower_triangle_indices]
+
+    model_interactions_vec = (distance_vec ** dd_power) * state_interactions_vec
+    probability_normalizing_constant = np.sum(model_interactions_vec)
+
+    log_likelihood = np.dot(interactions, np.log(model_interactions_vec/probability_normalizing_constant))
+
+    return -log_likelihood
+
+def clean_matrix(interactions_mat):
+    """
+    Perform various arrangements, cleaning and normalizations on the input interactions matrix. Removes empty bins,
+    ensures matrix is symmetric, etc.
+
+    :param array interactions_mat: Interactions matrix
+    :return: Clean Interactions matrix
+    :rtype: array
+    """
+    # fill the diagonal with nan values
+    clean_mat = interactions_mat.copy()
+    np.fill_diagonal(clean_mat, np.nan) 
+
+    # delete empty rows and columns
+    non_nan_mask = ~np.isnan(interactions_mat).all(1)
+    clean_mat = clean_mat[:, non_nan_mask][non_nan_mask, :]
+
+    # make sure we have a symmetric cis interactions matrix
+    clean_mat = np.maximum(clean_mat, clean_mat.transpose())
+
+    number_of_bins = np.size(clean_mat, 0)
+
+    lower_triangle_indices = np.tril_indices(number_of_bins, -1)
+    # normalizing the cis interaction matrix to sum of 1 for easier gradient evaluation
+    clean_mat /= np.nansum(clean_mat[lower_triangle_indices]) 
+
+    return clean_mat
+
+def get_unique_interactions(interaction_mat):
+    """
+    Return the lower triangle of the given matrix as a vector, resulting in a vector of only unique
+    interactions (as the upper triangle is symmetric to the lower)
+
+    :param array interaction_mat: the interaction matrix, preferably after running clean_matrix()
+    :return: A vector of the lower triangle's values
+    :rtype: 1d array
+    """
+    number_of_bins = np.size(interaction_mat, 0)
+    lower_triangle_indices = np.tril_indices(number_of_bins, -1)
+    value_vec = interaction_mat[lower_triangle_indices]
+
+    return value_vec
+
+def init_variables(probabilities_params_count, weights_param_count):
+    """
+    Give an intial value to all the variables we optimize.
+    """
+    prob_init = (np.random.rand(probabilities_params_count) / 2) + 0.25
+    weights_init = np.ones(weights_param_count) / weights_param_count
+    dd_init = (-1,)
+    x0 = np.concatenate((prob_init, weights_init, dd_init))
+
+    return x0
+
+def init_bounds(probabilities_params_count, weights_param_count):
+    """
+    Set bound for the probability, state weights and distance decay power variables
+    """
+    prob_bounds = [(0.01, 0.99)] * probabilities_params_count 
+    weights_bounds = [(0.01, 0.99)] * weights_param_count
+    dd_bounds = [(-2, -0.5)]
+    bounds = np.concatenate((prob_bounds, weights_bounds, dd_bounds), axis=0)  
+
+    return bounds
+
+def extract_variables(probabilities_params_count, weights_param_count, variables):
+    """
+    Extract the three groups of variables from a single 1D vector
+    """
+    weights_start_idx = probabilities_params_count
+    weights_end_idx = weights_start_idx + weights_param_count
+    dd_idx = weights_end_idx
+
+    prob_vars = variables[:probabilities_params_count]
+    weights_vars = variables[weights_start_idx:weights_end_idx]
+    dd_var = variables[dd_idx]
+
+    return prob_vars, weights_vars, dd_var
+
+def normalize(array, normalize_axis=None):
+    """
+    Normalize the given array by dividing by array.sum(axis=normalize_axis). This is used to get the
+    'real' values of values that have some normalization constraint (such as probabilities). We do 
+    this to bypass a L-BFGS-B limitation that only supports bound contraints.
+    """
+    normalization_factor = array.sum(axis=normalize_axis)[None].T # To make broadcasting work
+
+    return array/normalization_factor
+
+def triangle_to_symmetric(matrix_size, tri_values):
+    """
+    Convert the lower triangle values given by tri_values to a symmetric matrix with the given size
+    """
+    # This implementation is a bit more complicated than expected, because autograd doesn't support
+    # array assignments (so symmat[x,y] = symmat[y,x] = values won't work). Instead we build the matrix one
+    # column at a time
+    x, y = np.tril_indices(matrix_size)
+    def get_values(index):
+        return tri_values[(x == index) | (y == index)]
+
+    symmat = np.array([get_values(i) for i in range(matrix_size)])
+    return symmat
+
+def fit(interactions_mat, number_of_states=2):
+    """
+    Return the model parameters that best explain the given Hi-C interaction matrix using L-BFGS-B.
+
+    :param array interaction_mat: the Hi-C interaction matrix as a numpy array
+    :param int number_of_states: the number of possible states (compartments) for each bin
+    :return: A tuple of each bin's state probability (shape BINSxSTATES), state-state interaction
+             probabiity (shape STATES-STATES) and the distance-decay power value (scalar)
+    :rtype: tuple
+    """
+    original_number_of_bins = np.size(interactions_mat, 0) 
+    clean_interactions_mat = clean_matrix(interactions_mat)
+    new_number_of_bins = np.size(clean_interactions_mat, 0)
+    unique_interactions = get_unique_interactions(clean_interactions_mat)
+    non_nan_mask = ~np.isnan(interactions_mat).all(1)
+    non_nan_indices = np.where(non_nan_mask)[0]
+
+    probabilities_params_count = new_number_of_bins * number_of_states
+    weights_param_count = np.tril_indices(number_of_states)[0].size
+    distance_decay_param_count = 1
+
+    x0 = init_variables(probabilities_params_count, weights_param_count)
+    bounds = init_bounds(probabilities_params_count, weights_param_count)
+    optimize_options = dict(disp=True, ftol=1.0e-10, gtol=1e-010, eps=1e-10, maxfun=100000, maxiter=100000, maxls=100)
+
+    likelihood_with_model = functools.partial(calculate_likelihood, unique_interactions, non_nan_indices,
+            number_of_states, probabilities_params_count, weights_param_count, new_number_of_bins)
+    likelihood_grad = grad(likelihood_with_model)
+    res = sp.optimize.minimize(fun=likelihood_with_model, x0=x0, method='L-BFGS-B', jac=likelihood_grad, bounds=bounds,
+            options=optimize_options)
+
+    prob_vars, weights_vars, model_dd_power = extract_variables(probabilities_params_count, weights_param_count, res.x)
+    model_probabilities = normalize(prob_vars.reshape(new_number_of_bins, number_of_states), normalize_axis=1)
+    model_state_weights = normalize(triangle_to_symmetric(number_of_states, weights_vars))
+    expanded_model_probabilities = np.full((original_number_of_bins, number_of_states), np.nan)
+    expanded_model_probabilities[non_nan_indices, :] = model_probabilities
+    
+    return expanded_model_probabilities, model_state_weights, model_dd_power
+
+def generate_interactions_matrix(state_probabilities, state_weights, distance_decay_power):
+    """
+    Generate an NxN interactions matrix by using the given model parameters. Diagonal values will be
+    NaN as they're aren't modeled, along with any bins that have NaN in their state probabilities.
+
+    :param array state_probabilities: NxM matrix, N being the bin count and M the number of states
+    :param array state_weights: MxM symmetric matrix for state-state interaction probability
+    :param number distance_decay_power: The rate by which interaction rate decreases with distance
+    :return: interaction matrix generated by the model
+    :rtype: NxN array
+    """
+    number_of_bins = state_probabilities.shape[0]
+    compartments_interactions = state_probabilities.dot(state_weights).dot(state_probabilities.T)
+
+    distance_matrix = 1.0*np.absolute(np.arange(number_of_bins)[None].T - np.arange(number_of_bins))
+    np.fill_diagonal(distance_matrix, np.nan)
+    distance_decay_interactions = distance_matrix ** distance_decay_power
+    unnormalized_all_interactions = distance_decay_interactions * compartments_interactions
+    normalization_factor = np.nansum(np.tril(unnormalized_all_interactions, k=-1))
+
+    return unnormalized_all_interactions / normalization_factor
--- gc_model_main.py
+++ gc_model_main.py
@@ -1,71 +1,11 @@
-from __future__ import division
-import numpy as np
-import scipy as sp
-from scipy import optimize
-import time
 import argparse
 import cooler
-import matplotlib
 import os
-matplotlib.use('Agg')
+import sys
+import time
+import numpy as np
 
-start_time = time.time()  # Initial time point to measure function performances
-
-###################################################################################################
-# The "evaluation_of_likelihood_gradient" function calculates the likelihood of a given vector of #
-# compartments belonging probabilities and the distance decay power value.                        #
-# It also calculates the first order derivative for the gradient decent step of the algorithm.    #
-###################################################################################################
-
-def evaluation_of_likelihood_gradient(variables, *args):
-    
-    experimented_cis_interactions_is_not_nan_vec = args[1]
-    
-    arange_of_not_nan_locations = np.arange(np.size(experimented_cis_interactions_is_not_nan_vec))[experimented_cis_interactions_is_not_nan_vec]
-
-    number_of_bins = np.size(variables) - 1  
-
-    probabilities_vector = variables[0:number_of_bins]  
-
-    distance_decay_power = variables[number_of_bins]  
-
-    lower_triangular_experimented_cis_interactions = args[0] 
-
-    lower_triangular_distance_matrix = (arange_of_not_nan_locations[None].T-arange_of_not_nan_locations)[np.tril_indices(number_of_bins,-1)]
-
-    lower_triangular_cis_interactions_model_with_no_dd = (np.outer(probabilities_vector, probabilities_vector) + np.outer(1 - probabilities_vector, 1 - probabilities_vector))[np.tril_indices(number_of_bins,-1)]
-
-    lower_triangular_cis_interactions_model = np.multiply(lower_triangular_distance_matrix ** distance_decay_power, lower_triangular_cis_interactions_model_with_no_dd)
-    
-    probability_normalizing_constant = 1.0 / np.nansum(lower_triangular_cis_interactions_model)
-
-    gradient_of_current_distance_decay_power_likelihood = np.nansum(np.multiply(np.log(lower_triangular_distance_matrix),lower_triangular_cis_interactions_model)) * probability_normalizing_constant - np.nansum(np.multiply(lower_triangular_experimented_cis_interactions, np.log(lower_triangular_distance_matrix)))
-
-    cis_interactions_likelihood = np.nansum(np.multiply(lower_triangular_experimented_cis_interactions, np.log(probability_normalizing_constant * lower_triangular_cis_interactions_model)))
-        
-    cis_interactions_model_with_no_dd = np.outer(probabilities_vector, probabilities_vector) + np.outer(1 - probabilities_vector, 1 - probabilities_vector)
-
-    likelihood_of_current_probabilities_vector = -1.0 * cis_interactions_likelihood
-
-    #should try to improve this line that replaces the following 5 lines:
-    #gradient_of_current_probabilities_vector_likelihood = np.nansum((2.0 * lower_triangular_distance_matrix ** distance_decay_power) * (2.0 * probabilities_vector - np.ones(number_of_bins)) * probability_normalizing_constant, axis=1) - np.nansum(((lower_triangular_experimented_cis_interactions * (2.0 * probabilities_vector - np.ones(number_of_bins))) * (1.0 / cis_interactions_model_with_no_dd)), axis=1)
-        
-    full_distance_matrix = (np.absolute(arange_of_not_nan_locations-arange_of_not_nan_locations[None].T) + np.diag(np.zeros(number_of_bins)+np.nan))
-    gradient_of_current_probabilities_vector_likelihood_part1 = np.nansum((full_distance_matrix ** distance_decay_power) * (2.0 * probabilities_vector - np.ones(number_of_bins)) * probability_normalizing_constant, axis=1) 
-
-    full_experimented_cis_interactions = cis_interactions_model_with_no_dd*0
-    full_experimented_cis_interactions[np.tril_indices(number_of_bins,-1)] = lower_triangular_experimented_cis_interactions
-    full_experimented_cis_interactions = full_experimented_cis_interactions + full_experimented_cis_interactions.transpose()
-    gradient_of_current_probabilities_vector_likelihood_part2 = - np.nansum(((full_experimented_cis_interactions * (2.0 * probabilities_vector - np.ones(number_of_bins))) * (1.0 / cis_interactions_model_with_no_dd)), axis=1)
-
-    gradient_of_current_probabilities_vector_likelihood = gradient_of_current_probabilities_vector_likelihood_part1 + gradient_of_current_probabilities_vector_likelihood_part2
-
-    final_likelihood_gradient_for_evaluation_of_likelihood_gradient_function = np.append(gradient_of_current_probabilities_vector_likelihood, [gradient_of_current_distance_decay_power_likelihood])
-
-    final_likelihood_gradient_for_evaluation_of_likelihood_gradient_function = np.nan_to_num(final_likelihood_gradient_for_evaluation_of_likelihood_gradient_function)
-
-    return likelihood_of_current_probabilities_vector, final_likelihood_gradient_for_evaluation_of_likelihood_gradient_function
-
+from gc_model import fit
 
 def get_matrix_from_coolfile(mcool_filename, experiment_resolution, chromosome):
     """
@@ -84,116 +24,62 @@ def get_matrix_from_coolfile(mcool_filename, experiment_resolution, chromosome):
 
     return experimented_cis_interactions
 
-def save_input_data(interactions_mat, output_dir, output_filename_suffix=''):
-    """
-    Save to disk the interactions matrix and the original indices of non-NaN values.
-    This can be used for later correlation evaluation with bio tracks.
-
-    The input matrix filename is 'input_matrix{suffix}.npz'
-    The non-NaN indices filename is 'experimented_cis_interactions_is_not_nan_vec{suffix}.npy'
-
-    :param numpy-array interactions_mat: Interactions (Hi-C) matrix
-    :param str chromosome: chromosome used for this experiment (chrXX)
-    :param int resolution: matrix resolution (bin size)
-    :param str output_dir: directory where output files will be saved
-    """
-    non_nan_indices = ~np.isnan(interactions_mat).all(1)
-    non_nan_output_filename = f'experimented_cis_interactions_is_not_nan_vec{output_filename_suffix}.npy'
-    np.save(os.path.join(output_dir, non_nan_output_filename), non_nan_indices)
-
-    input_matrix_filename = f'input_matrix{output_filename_suffix}'
-    np.savez_compressed(os.path.join(output_dir, input_matrix_filename), a=interactions_mat)
-
-def save_output_data(probabilities_vector, distance_decay_power, output_dir, output_filename_suffix):
-    filename_dd_power_value = f'model_distance_decay_power_value{output_filename_suffix}.npy'
-    filename_probabilities = f'lambda_probabilities{output_filename_suffix}.npy'
-    np.save(os.path.join(output_data_dir, filename_dd_power_value), distance_decay_power)  
-    np.save(os.path.join(output_data_dir, filename_probabilities), probabilities_vector)
-
-def matrix_to_value_vector(input_interactions_mat):
-    """
-    Convert the input interactions matrix into a 1D vector of the observed values.
-    The vector is taken from the lower triangle of the interactions matrix after various arrangements, cleaning and normalizations.
-
-    :param array input_interactions_mat: Interactions matrix
-    :return: A tuple of the vector of interaction values and the new number of bins (after empty ones were removed)
-    :rtype: tuple
-    """
-    # fill the diagonal with nan values
-    clean_mat = input_interactions_mat.copy()
-    np.fill_diagonal(clean_mat, np.nan) 
-
-    # delete empty rows and columns
-    non_nan_indices = ~np.isnan(input_interactions_mat).all(1)
-    clean_mat = clean_mat[:, non_nan_indices][non_nan_indices, :]
-
-    # make sure we have a symmetric cis interactions matrix
-    clean_mat = np.maximum(clean_mat, clean_mat.transpose())
-
-    number_of_bins = np.size(clean_mat, 0)
-
-    lower_triangle_indices = np.tril_indices(number_of_bins, -1)
-    # normalizing the cis interaction matrix to sum of 1 for easier gradient evaluation
-    clean_mat /= np.nansum(clean_mat[lower_triangle_indices]) 
-
-    # keep only the lower triangular interaction matrix
-    value_vec = clean_mat[lower_triangle_indices]
-
-    return value_vec, number_of_bins
-
-def process_matrix(input_interactions_mat):
-    non_nan_indices = ~np.isnan(input_interactions_mat).all(1)
-
-    original_number_of_bins = np.size(input_interactions_mat, 0) 
-    values_vec, new_number_of_bins = matrix_to_value_vector(input_interactions_mat)
-
-    # set initial random vector for the algorithm
-    x0_random = np.append((np.random.rand(new_number_of_bins) / 2) + 0.25, [-1])  
-
-    # set bound for the probability vector and distance decay power
-    bnd = np.append([(0.01, 0.99)] * new_number_of_bins, [(-2, -0.5)], axis=0)  
-
-    # calculate the maximal likelihood probabilities vector and distance decay power using the "L-BFGS-B" method
-    optimize_options = dict(disp=True, ftol=1.0e-10, gtol=1e-010, eps=1e-10, maxfun=100000, maxiter=100000, maxls=100)
-    res = sp.optimize.minimize(fun=evaluation_of_likelihood_gradient, x0=x0_random, args=(values_vec, non_nan_indices),
-            method='L-BFGS-B', jac=True, bounds=bnd, options=optimize_options)
-
-    # resulted probabilities vector and distance decay power of the model
-    model_probabilities_vector = np.zeros(original_number_of_bins) + np.nan
-    model_probabilities_vector[non_nan_indices] = res.x[0:new_number_of_bins] 
-    
-    model_distance_decay_power_value = res.x[new_number_of_bins]
-
-    return model_probabilities_vector, model_distance_decay_power_value
+def detect_file_type(filename):
+    if filename.endswith('.mcool'):
+        return 'mcool'
+    elif filename.endswith('.npy'):
+        return 'numpy'
+    else:
+        raise ValueError('unknown file type')
 
 def main():
-
     #################################################################################################
     # Calculating the maximal likelihood probabilities vector and distance decay power of the model #
     #################################################################################################
 
     parser = argparse.ArgumentParser(description = 'Experiment number (diferent initial random vectors)')
-    parser.add_argument('-m',help='mcool file name',dest='mcool',type=str,required=True)
-    parser.add_argument('-dr',help='directory name',dest='directory',type=str,required=True)
-    parser.add_argument('-ch',help='chromosome',dest='chrom',type=str,required=True)
-    parser.add_argument('-l',help='local directory path',dest='local',type=str,required=True)
-    parser.add_argument('-kb',dest='resolution',type=int,required=True)
+    parser.add_argument('-m', help='file name', dest='filename', type=str, required=True)
+    parser.add_argument('-t', help='file type', dest='type', type=str, choices=['mcool, numpy, auto'], default='auto')
+    parser.add_argument('-d', help='output directory name', dest='directory', type=str, required=False, default='.')
+    parser.add_argument('-ch', help='chromosome (required for type=mcool)', dest='chrom', type=str, required=False)
+    parser.add_argument('-kb', help='resolution (required for type=mcool)', dest='resolution', type=int, required=False)
+    parser.add_argument('-n', help='number of states', dest='nstates', type=int, required=False, default=2)
     args = parser.parse_args()
     
-    local = str(args.local)
-    chrom = f'chr{args.chrom}'
-    directory = args.directory
-    mcool_filename = args.mcool
-    experiment_resolution = args.resolution
-    output_data_dir = os.path.join(local, directory, 'OutputData')
-    output_filename_suffix = f'_{chrom}_{experiment_resolution}kb'
+    filename = args.filename
+    file_type = args.type
+    if file_type == 'auto':
+        file_type = detect_file_type(filename)
+    if file_type == 'mcool' and (args.chrom is None or args.resolution is None):
+        print("chromosome and resolution must be given for mcool files")
+        sys.exit(1)
+    output_dir = args.directory
+    nstates = args.nstates
+    if file_type == 'mcool':
+        chrom = f'chr{args.chrom}'
+        experiment_resolution = args.resolution
+        output_filename_suffix = f'_{chrom}_{experiment_resolution}kb_{nstates}states'
+        interactions_mat = get_matrix_from_coolfile(filename, experiment_resolution, chrom)
+    else:
+        interactions_mat = np.load(filename)
+        output_filename_suffix = f'_{nstates}states'
 
-    input_interactions_mat = get_matrix_from_coolfile(mcool_filename, experiment_resolution, chrom)
-    save_input_data(input_interactions_mat, output_data_dir, output_filename_suffix)
-    model_probabilities_vector, model_distance_decay_power_value = process_matrix(input_interactions_mat)
-    save_output_data(model_probabilities_vector, model_distance_decay_power_value, output_data_dir, output_filename_suffix)
+    print(f'Fitting {filename} to model with {nstates} states.')
+    non_nan_mask = ~np.isnan(interactions_mat).all(1)
+    np.save(os.path.join(output_dir, f'experimented_cis_interactions_is_not_nan_vec{output_filename_suffix}.npy'), non_nan_mask)
+    np.savez_compressed(os.path.join(output_dir, f'input_matrix{output_filename_suffix}'), a=interactions_mat)
 
-    print(f'Data saved into directory {output_data_dir} with suffix {output_filename_suffix}.')
+    start_time = time.time()
+    probabilities_vector, state_weights, distance_decay_power_value = fit(interactions_mat, number_of_states=nstates)
+    end_time = time.time()
+    print(f'Took {end_time - start_time} seconds')
+
+    np.save(os.path.join(output_dir, f'lambda_probabilities{output_filename_suffix}.npy'), probabilities_vector)
+    np.save(os.path.join(output_dir, f'model_distance_decay_power_value{output_filename_suffix}.npy'),
+            distance_decay_power_value)
+    np.save(os.path.join(output_dir, f'model_state_weights{output_filename_suffix}.npy'), state_weights)
+
+    print(f'Data saved into directory {output_dir} with suffix {output_filename_suffix}.')
  
 if __name__ == '__main__':
     main()
