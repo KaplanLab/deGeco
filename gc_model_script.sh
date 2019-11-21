4ce482ec576871f66f7a88b136a073a589de586a
Hagai Kariti <hkariti@gmail.com>
2019-11-21 17:42:38 +0200

    update gc_model_script to new gc_model_main

--- gc_model_script.sh
+++ gc_model_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-/Local/md_kaplan/anaconda3/bin/python -u /storage/md_kaplan/haiak/Tal_model/Scripts/gc_model_main.py -kb $r1 -m $m1 -dr $d1 -ch $ch1 -l $l1
+/Local/md_kaplan/anaconda3/bin/python -u /storage/md_kaplan/haiak/Tal_model/Scripts/gc_model_main.py -kb $r1 -m $m1 -d $l1/$d1 -ch $ch1
 
