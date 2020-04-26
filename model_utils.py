2b53ca1c8a8f825f92c1bebaed4aaa0fdf529ae3
Hagai Kariti <hkariti@gmail.com>
2020-04-26 12:26:56 +0300

    Ignore nan interactions when fitting
    This allows to remove interactions that can bias the model by nan-ing
    them, e.g. remove distant interactions when fitting the insulation model

--- model_utils.py
+++ model_utils.py
@@ -2,9 +2,13 @@ import autograd.numpy as np
 from autograd.scipy.special import logsumexp
 
 def log_likelihood(x, log_p):
-    log_z = logsumexp(log_p)
+    x_mask = np.isfinite(x)
+    usable_x = x[x_mask]
+    usable_log_p = log_p[x_mask]
 
-    return np.sum(np.multiply(x, log_p - log_z))
+    log_z = logsumexp(usable_log_p)
+
+    return np.sum(np.multiply(usable_x, usable_log_p - log_z))
 
 def expand_by_mask(a, mask, blank_value=np.nan):
     target_shape = list(a.shape)
