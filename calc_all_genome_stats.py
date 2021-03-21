e2b12bdfcfd3fd4ca0a14f29424964c5a589a9ad
Hagai Kariti <hkariti@gmail.com>
2021-03-21 22:38:45 +0200

    Add scripts to calculate stats for figures

---
+++ calc_all_genome_stats.py
@@ -0,0 +1,74 @@
+import numpy as np
+from scipy import stats
+import time
+
+import gc_model as gc
+import array_utils
+import hic_analysis as hic
+from toolz.curried import *
+
+load_fit = lambda f: np.load(f, allow_pickle=True)['parameters'][()]
+fit_to_mat = lambda fit: gc.generate_interactions_matrix(**fit)
+
+@curry
+def likelihood(interactions_mat, fit_mat, mask=True):
+    clean_interactions_mat = array_utils.normalize_tri_l1(gc.preprocess(interactions_mat))
+    unique_interactions = array_utils.get_lower_triangle(clean_interactions_mat)
+    clean_fit_mat = array_utils.normalize_tri_l1(gc.preprocess(fit_mat))
+    unique_fit_interactions = array_utils.get_lower_triangle(clean_fit_mat)
+    log_unique_fit_interactions = np.log(unique_fit_interactions)
+    nn = np.isfinite(unique_interactions) & np.isfinite(log_unique_fit_interactions) & mask
+
+    return np.sum(unique_interactions[nn] * log_unique_fit_interactions[nn])
+
+@curry
+def likelihood_ratio(interactions_mat, fit_mat):
+    return np.exp(likelihood(interactions_mat, fit_mat) - likelihood(interactions_mat, interactions_mat))
+
+@curry
+def calc_pearsonr(mat1, mat2):
+    nn = np.isfinite(mat1) & np.isfinite(mat2)
+    return stats.pearsonr(mat1[nn], mat2[nn])[0]
+
+@curry
+def calc_spearmanr(mat1, mat2):
+    nn = np.isfinite(mat1) & np.isfinite(mat2)
+    return stats.spearmanr(mat1[nn], mat2[nn])[0]
+
+def log(msg):
+    print(time.ctime(), ":", msg)
+
+def calc_stats(all_mat, states, instances=10):
+    pearson_array = np.empty((len(states), instances))
+    spearman_array = np.empty((len(states), instances))
+    lr_array = np.empty((len(states), instances))
+    for i, s in enumerate(states):
+        log(f"Calculating stats for state {s}")
+        for k in range(instances):
+            log(f"Starting run {k+1}")
+            try:
+                fit = load_fit("/srv01/technion/hagaik/storage/paper/runs/all_diag_{s}st_100000_run{k+1}.npz")
+            except FileNotFoundError:
+                print("Not found, skipping")
+                pearson_array[i, k] = spearman_array[i, k] = lr_array[i, k] = np.nan
+                continue
+            fit_mat = fit_to_mat(fit)
+            pearson_array[i, k] = calc_pearsonr(all_mat, fit_mat)
+            log(f"Pearson: {pearson_array[i, k]}")
+            spearman_array[i, k] = calc_spearmanr(all_mat, fit_mat)
+            log(f"Spearman: {spearman_array[i, k]}")
+            lr_array[i, k] = likelihood_ratio(all_mat, fit_mat)
+            log(f"LR: {lr_array[i, k]}")
+    return dict(pearson=pearson_array, spearman=spearman_array, lr=lr_array)
+            
+
+log("Reading source matrix")
+all_mat = hic.preprocess(hic.get_matrix_from_coolfile("/storage/md_kaplan/SharedData/Rao-Cell-2015/cool/Rao2014-GM12878-MboI-allreps-filtered.mcool", 100000, 'all'))
+chr_lengths = hic.get_chr_lengths("/storage/md_kaplan/SharedData/Rao-Cell-2015/cool/Rao2014-GM12878-MboI-allreps-filtered.mcool", 100000, ['all'])
+
+
+st_range = list(range(2, 9))
+stats = calc_stats(all_mat, st_range)
+output_file = '/srv01/technion/hagaik/storage/paper/runs/all_genome_stats.npy'
+log(f"Saving file to {output_file}")
+np.savez_compressed(output_file, **stats)
---
+++ calc_sc_stats.py
@@ -0,0 +1,109 @@
+import warnings
+import numpy as np
+import os
+import glob
+import matplotlib.pyplot as plt
+import matplotlib
+import itertools
+from scipy import stats
+import subprocess
+import sys
+
+import gc_model as gc
+import array_utils
+import hic_analysis as hic
+from toolz.curried import *
+
+run_dir = sys.argv[1]
+fit_to_mat = lambda fit: gc.generate_interactions_matrix(**fit)
+sc_hic = lambda s, res, reads: np.load(f"{run_dir}/no_regularization_newformat/fit_chr10_{s}st_res{res}_reads{reads}_best.npz", allow_pickle=True)['parameters'][()]
+orig = lambda s, res: np.load(f"{run_dir}/orig_chr10_{s}st_{res}_newformat.npz", allow_pickle=True)['parameters'][()]
+
+st = 2
+resolutions = [40000, 50000, 100000, 250000, 500000, 1000000]
+reads = [100000, 250000, 500000, 750000, 1000000]
+
+@curry
+def likelihood(interactions_mat, fit_mat, mask=True):
+    clean_interactions_mat = array_utils.normalize_tri_l1(gc.preprocess(interactions_mat))
+    unique_interactions = array_utils.get_lower_triangle(clean_interactions_mat)
+    clean_fit_mat = array_utils.normalize_tri_l1(gc.preprocess(fit_mat))
+    unique_fit_interactions = array_utils.get_lower_triangle(clean_fit_mat)
+    log_unique_fit_interactions = np.log(unique_fit_interactions)
+    nn = np.isfinite(unique_interactions) & np.isfinite(log_unique_fit_interactions) & mask
+
+    return np.sum(unique_interactions[nn] * log_unique_fit_interactions[nn])
+
+@curry
+def likelihood_ratio(interactions_mat, fit_mat):
+    nz = array_utils.get_lower_triangle((interactions_mat != 0) & (fit_mat != 0), k=-1)
+    return np.exp(likelihood(interactions_mat, fit_mat, nz) - likelihood(interactions_mat, interactions_mat, nz))
+
+@curry
+def calc_pearsonr(mat1, mat2):
+    nn = np.isfinite(mat1) & np.isfinite(mat2)
+    return stats.pearsonr(mat1[nn], mat2[nn])[0]
+
+@curry
+def calc_spearmanr(mat1, mat2):
+    nn = np.isfinite(mat1) & np.isfinite(mat2)
+    return stats.spearmanr(mat1[nn], mat2[nn])[0]
+
+def gc_fit_newformat(fit):
+    gc = fit['state_probabilities'] @ fit['state_weights'] @ fit['state_probabilities'].T
+    np.fill_diagonal(gc, np.nan)
+    return array_utils.normalize_tri_l1(gc)
+
+
+def calc_sc_stats_newformat(st, resolutions, reads):
+    pearson_corrs = np.empty((len(resolutions), len(reads)))
+    spearman_corrs = np.empty((len(resolutions), len(reads)))
+    lambda_mean_diffs = np.empty((len(resolutions), len(reads)))
+    lr = np.empty((len(resolutions), len(reads)))
+    normalized_pearsonr = np.empty((len(resolutions), len(reads)))
+    gc_correlation = np.empty((len(resolutions), len(reads)))
+    for i, res in enumerate(resolutions):
+        print(f"* Resolution: {res}")
+        print("** Loading original fit:", end=" ")
+        orig_fit = orig(st, res)
+        orig_mat = fit_to_mat(orig_fit)
+        orig_normalized = hic.normalize_distance(orig_mat)
+        print("done")
+        for j, r in enumerate(reads):
+            print(f"** Reads: {r}")
+            print(f"** Loading fit:", end=" ")
+            sc_fit = sc_hic(st, res, r)
+            sc_mat = gc.generate_interactions_matrix(**sc_fit)
+            sc_normalized = hic.normalize_distance(sc_mat)
+            print("done")
+            print("*** Pearson:", end=' ')
+            pearson_corrs[i, j] = calc_pearsonr(orig_mat, sc_mat)
+            print(pearson_corrs[i, j])
+            print("*** Spearman:", end=' ')
+            spearman_corrs[i, j] = calc_spearmanr(orig_mat, sc_mat)
+            print(spearman_corrs[i, j])
+            print("*** Lambda mean diff:", end=' ')
+            lambda_mean_diffs[i, j] = np.minimum(
+                                        np.nanmean(np.abs(sc_fit['state_probabilities'][:, 0] - orig_fit['state_probabilities'][:, 0])),
+                                        np.nanmean(np.abs(sc_fit['state_probabilities'][:, 0] - orig_fit['state_probabilities'][:, 1]))
+                                    )
+            print(lambda_mean_diffs[i, j])
+            print("*** Normalized pearson:", end=' ')
+            normalized_pearsonr[i, j] = calc_pearsonr(orig_normalized, sc_normalized)
+            print(normalized_pearsonr[i, j])
+            print("*** LR:", end=' ')
+            lr[i, j] = likelihood_ratio(orig_mat, sc_mat)
+            print(lr[i, j])
+            print("*** GC-only pearson", end=' ')
+            gc_correlation[i, j] = calc_pearsonr(gc_fit_newformat(orig_fit), gc_fit_newformat(sc_fit))
+            print(gc_correlation[i, j])
+    return dict(pearson_corrs=pearson_corrs, spearman_corrs=spearman_corrs,
+                normalized_pearson_corrs=normalized_pearsonr, lr=lr, lambda_mean_diffs=lambda_mean_diffs,
+                gc_correlation=gc_correlation)
+
+print(f"Using run dir: {run_dir}")
+sc_stats_newformat = calc_sc_stats_newformat(2, resolutions, reads)
+output_file=f"{run_dir}/no_regularization_newformat/stats_best.npz"
+print("Saving to file", output_file)
+np.savez(output_file, **sc_stats_newformat)
+print("Done")
